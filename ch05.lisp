31c5cea7cd7157821a352320dcae37e04f0e6e58
quinton <18607942672@163.com>
2019-01-14 13:55:51 +0800

    ch05 is doing!

--- ch05.lisp
+++ ch05.lisp
@@ -21,13 +21,49 @@
     (feb (if (leap-year) 29 28))
     (otherwise "unknown month")))
 
-(defun show-squares (start end)
+(defun show-squares (start end);;打印数的平方
   (do ((i start (+ i 1)))
       ((> i end) 'done)
     (format t "~A ~A~%" i (* i i))))
 
-(defun factorial (n)
+(defun factorial (n);;计算阶乘
   (do ((j n (- j 1))
        (f 1 (* j f)))
       ((= j 0) f)))
+
+(defun sub ();;抛出异常
+  (throw 'abort 99))
+
+(defun super ();;捕获异常
+  (catch 'abort
+    (sub)
+    (format t "we'll never see this.")))
+
+(defconstant month;;定义每个月开始的那一天是一年中的第几天
+  #(0 31 59 90 120 151 181 212 243 273 304 334 365))
+
+(defconstant yzero 2000);;从2000开始计算
+
+(defun leap? (y);;判断是否为闰年
+  (and (zerop (mod y 4))
+       (or (zerop (mod y 400))
+           (not (zerop (mod y 100))))))
+
+(defun month-num (m y);;根据月份计算该月第一天是全年的第几天
+  (+ (svref month (- m 1))
+     (if (and (> m 2) (leap? y)) 1 0)))
+
+(defun year-days (y);;计算一年有多少天
+  (if (leap? y) 366 365))
+
+(defun year-num (y);;计算y年距2000年有多少天
+  (let ((d 0))
+    (if (>= y yzero)
+        (dotimes (i (- y yzero) d)
+          (incf d (year-days (+ yzero i))))
+        (dotimes (i (- yzero y) (- d))
+          (incf d (year-days (+ y i)))))))
+
+(defun date->num (d m y);;计算y年m月d日距2000年1月1日有多少天
+  (+ (- d 1) (month-num m y) (year-num y)))
 
