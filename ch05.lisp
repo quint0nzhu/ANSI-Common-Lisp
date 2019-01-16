b58e92d4284c2842991794543ac8ba6f10c0558c
18607942672 <18607942672@163.com>
2019-01-15 23:26:29 -0500

    ch05 is done!

--- ch05.lisp
+++ ch05.lisp
@@ -64,6 +64,35 @@
         (dotimes (i (- yzero y) (- d))
           (incf d (year-days (+ y i)))))))
 
-(defun date->num (d m y);;计算y年m月d日距2000年1月1日有多少天
+(defun date->num (d m y);;计算y年m月d日距2000年1月1日有多少天，转换日期至数字
   (+ (- d 1) (month-num m y) (year-num y)))
+
+(defun nmon (n);;根据一年中的第几天转换成几月几日
+  (let ((m (position n month :test #'<)))
+    (values m (+ 1 (- n (svref month (- m 1)))))))
+
+(defun num-month (n y);;根据年份和天数，分解出月与日
+  (if (leap? y)
+      (cond ((= n 59) (values 2 29))
+            ((> n 59) (nmon (- n 1)))
+            (t        (nmon n)))
+      (nmon n)))
+
+(defun num-year (n);;以日期的格式返回年，以及剩余的天数
+  (if (< n 0)
+      (do* ((y (- yzero 1) (- y 1))
+            (d (- (year-days y)) (- d (year-days y))))
+           ((<= d n) (values y (- n d))))
+      (do* ((y yzero (+ y 1))
+            (prev 0 d)
+            (d (year-days y) (+ d (year-days y))))
+           ((> d n) (values y (- n prev))))))
+
+(defun num->date (n);;将整数转换回日期
+  (multiple-value-bind (y left) (num-year n)
+    (multiple-value-bind (m d) (num-month left y)
+      (values d m y))))
+
+(defun date+ (d m y n);;日期加法，日期加上天数得到新的日期
+  (num->date (+ (date->num d m y) n)))
 
