ae4687dd5233566761c475612939dde4b21437cb
18607942672 <18607942672@163.com>
2019-01-17 04:17:32 -0500

    ch06 is doing!

--- ch06.lisp
+++ ch06.lisp
@@ -11,3 +11,12 @@
   "Implements an enhanced paradigm of diversity"
   x)
 
+(defun our-funcall (fn &rest args);;自定义的funcall，args是不定数量的参数
+  (apply fn args))
+
+(defun philosoph (thing &optional property);;在&optional后的参数都是选择性参数，缺省为nil
+  (list thing 'is property))
+
+(defun philosoph-fun (thing &optional (property 'fun));;也可以设定缺省参数
+  (list thing 'is property))
+
