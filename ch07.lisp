deaa9ae113594ffebbd00fbc7d6f2db660818b1f
18607942672 <18607942672@163.com>
2019-01-28 04:03:03 -0500

    ch07 is starting!

--- ch07.lisp
+++ ch07.lisp
@@ -2,3 +2,9 @@
 ;;date: 2019.01.25
 ;;by quinton from ANSI Common Lisp chapter 7
 
+(defun pseudo-cat (file)
+  (with-open-file (str file :direction :input)
+    (do ((line (read-line str nil 'eof)
+               (read-line str nil 'eof)))
+        ((eql line 'eof))
+      (format t "~A~%" line))))
