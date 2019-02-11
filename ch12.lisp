be2d00883b46d356b56c647d77cb594e245b4eee
quinton <18607942672@163.com>
2019-02-12 07:19:33 +0800

    ch12 is doing!

--- ch12.lisp
+++ ch12.lisp
@@ -219,3 +219,35 @@
                 (setf (node-l bst) (bst-delete obj (node-l bst) <))
                 (setf (node-r bst) (bst-delete obj (node-r bst) <)))
             bst))))
+
+(defstruct (dl (:print-function print-dl))
+  prev data next)
+
+(defun dl->list (lst)
+  (if (dl-p lst)
+      (cons (dl-data ls) (dl->list (dl-next lst)))
+      lst))
+
+(defun dl-insert (x lst)
+  (let ((elt (make-dl :data x :next lst)))
+    (when (dl-p lst)
+      (if (dl-prev lst)
+          (setf (dl-next (dl-prev lst)) elt
+                (dl-prev elt) (dl-prev lst)))
+      (setf (dl-prev lst) elt))
+    elt))
+
+(defun dl-list (&rest args)
+  (reduce #'dl-insert args
+          :from-end t :initial-value nil))
+
+(defun dl-remove (lst)
+  (if (dl-prev lst)
+      (setf (dl-next (dl-prev lst)) (dl-next lst)))
+  (if (dl-next lst)
+      (setf (dl-prev (dl-next lst)) (dl-prev lst)))
+  (dl-next lst))
+
+(defun print-dl (dl stream depth)
+  (declare (ignore depth))
+  (format stream "#<DL ~A>" (dl->list dl)))
