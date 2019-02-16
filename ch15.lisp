380138ce3e4e275063b1b979b9f34470915ea379
quinton <18607942672@163.com>
2019-02-16 21:05:46 +0800

    ch15 is doing!

--- ch15.lisp
+++ ch15.lisp
@@ -6,13 +6,13 @@
   (and (symbolp x)
        (eql (char (symbol-name x) 0) #\?)))
 
-(defun binding (x binds)
+(defun binding (x binds);;在一个列表中寻找一个变量所关联的值
   (let ((b (assoc x binds)))
     (if b
         (or (binding (cdr b) binds)
             (cdr b)))))
 
-(defun match (x y &optional binds)
+(defun match (x y &optional binds);;接受两棵树，如果能匹配则返回一个关联列表和一个布尔值
   (cond
     ((eql x y) (values binds t))
     ((assoc x binds) (match (binding x binds) y binds))
@@ -24,3 +24,54 @@
        (multiple-value-bind (b2 yes)
            (match (car x) (car y) binds)
          (and yes (match (cdr x) (cdr y) b2)))))))
+
+(defvar *rules* (make-hash-table));;包含规则的哈希表
+
+(defmacro <- (con &optional ant);;定义事实和规则，事实是没有body部分，规则可以通过证明body部分来证明head部分,返回给定判断式下存储的规则数量
+  `(length (push (cons (cdr ',con) ',ant)
+                 (gethash (car ',con) *rules*))))
+
+(defun vars-in (expr)
+  (if (atom expr)
+      (if (var? expr) (list expr))
+      (union (vars-in (car expr))
+             (vars-in (cdr expr)))))
+
+(defun change-vars (r)
+  (sublis (mapcar #'(lambda (v) (cons v (gensym "?")))
+                  (vars-in r))
+          r))
+
+(defun prove-simple (pred args binds)
+  (mapcan #'(lambda (r)
+              (multiple-value-bind (b2 yes)
+                  (match args (car r)
+                    binds)
+                (when yes
+                  (if (cdr r)
+                      (prove (cdr r) b2)
+                      (list b2)))))
+          (mapcar #'change-vars
+                  (gethash pred *rules*))))
+
+(defun prove-and (clauses binds)
+  (if (null clauses)
+      (list binds)
+      (mapcan #'(lambda (b)
+                  (prove (car clauses) b))
+              (prove-and (cdr clauses) binds))))
+
+(defun prove-or (clauses binds)
+  (mapcan #'(lambda (c) (prove c binds))
+          clauses))
+
+(defun prove-not (clause binds)
+  (unless (prove clause binds)
+    (list binds)))
+
+(defun prove (expr &optional binds)
+  (case (car expr)
+    (and (prove-and (reverse (cdr expr)) binds))
+    (or (prove-or (cdr expr) binds))
+    (not (prove-not (cadr expr) binds))
+    (t (prove-simple (car expr) (cdr expr) binds))))
